ca1b1eee383081d194be6c86039326221b730035
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-10-15 19:43:55 +0200

    QUIC proxy peering: a brief explanation

--- lib/proxy/peer/quic.go
+++ lib/proxy/peer/quic.go
@@ -47,3 +47,92 @@ const (
 	// are kept for at least twice this value.
 	quicNoncePersistence = 5 * time.Minute
 )
+
+/*
+
+# QUIC proxy peering
+
+QUIC proxy peering uses QUIC connections to the same address and port as the
+regular proxy peering listener (port 3021, by default) and use the same routing
+logic as the existing proxy peering.
+
+Until the feature is stabilized, a proxy can advertise support for receiving
+QUIC proxy peering connections through a label in its heartbeat. In the current
+implementation a proxy will only use outbound QUIC connections if it's also
+accepting QUIC connections, and all connections will use the same socket; this
+has the effect of taking up half the conntrack entries than TCP proxy peering
+(as each outbound TCP connection to a given destination needs use a different
+ephemeral port).
+
+## Protocol
+
+The server will accept connections from any Proxy of the cluster; in the
+connection, the client can open a bidirectional stream for each dial attempt
+through the server. The connections use ALPN with a protocol name that contains
+the "version" of the protocol (currently v1alpha, matching the protobuf package
+version).
+
+The client opens each stream by sending the protobuf binary encoding of a
+DialRequest message (see proto/teleport/quicpeering/v1alpha/dial.proto),
+length-prefixed by a little endian 32 bit integer. The server will check that
+the request is valid (see the "0-RTT considerations" section), attempt to dial
+the agent through a reverse tunnel, then report back an error in a DialResponse
+message that contains a google.rpc.Status (by taking the error and passing it
+through trace/trail, which is conveniently how we transfer errors on the wire
+with gRPC); the response is, like the request, encoded in binary protobuf format
+and length-prefixed.
+
+If the status is ok (signifying no error) then the stream will stay open,
+carrying the data for the connection between the user and the agent, otherwise
+the stream will be closed. For sanity's sake, the size of both messages is
+limited and any oversized message is treated as a protocol violation.
+
+## Multiplexing (or lack thereof)
+
+While the current server implementation poses no limits to the amount of streams
+in a single connection, real-world tests have shown that the best performance in
+terms of throughput and isolation between different user connections is achieved
+with individual QUIC connections between proxies. This would be very impractical
+with TCP, as that would result in significantly heavier load on the network
+infrastructure from having to keep track of all the individual TCP connections,
+but QUIC can use its own internal connection IDs as it sends and receives UDP
+packets over the same (source address, source port, destination address,
+destination port) 4-ple.
+
+As such, the current client implementation opens a new QUIC connection for each
+user connection, with a single stream in it. This could be changed without
+breaking compatibility in the future.
+
+## 0-RTT considerations
+
+QUIC can make use of TLS session resumption, to make TLS handshakes
+computationally cheaper. When resuming a session, the client can send data in
+the very first packets, without waiting for any data from the server. This
+"0-RTT" data is authentic and protected against eavesdropping, and any response
+from the server would likewise be protected against eavesdropping, but an
+attacker with the ability to sniff and inject data can blindly replay the
+initial resumed handshake including the 0-RTT data.
+
+Since proxy peering is at its most useful when dealing with connections across
+regions, it's very advantageous to take advantage of the latency reduction
+offered by 0-RTT; to prevent any problems caused by replay attacks, the client
+(when using 0-RTT data) must include a timestamp and a nonce in the request; the
+server will require a full handshake if the timestamp is too far off the local
+time, and then will check the nonce against a list of nonces received recently.
+If the nonce was already used, the dial request is rejected (potentially
+rejecting the legitimate dial attempt, which will increase the chance that
+someone will notice that something is wrong), otherwise the nonce is added to
+the list and the dial request is accepted. This makes it so that each dial
+request sent as 0-RTT can result in at most one connection opened through an
+agent tunnel.
+
+The client should make sure to not send data belonging to the connection as part
+of the early data; this will cause no further delays if the client intends to
+wait for the server to reply to the dial request.
+
+The protocol doesn't currently take advantage of early server-side data for
+non-resumed connections, so considerations around the security of "0.5-RTT" data
+are not relevant; data sent by the server as a response to the client is either
+using 0-RTT keys or is sent after the handshake is completed.
+
+*/
